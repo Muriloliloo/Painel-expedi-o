949b2be040f047136518c89f1ae925e2879017ab
Murilo da Costa Almeida de Jesus <murilo.costajesus@gmail.com>
2026-05-23 18:09:07 -0300

    Add files via upload

--- supabase-config.js
+++ supabase-config.js
@@ -1,5 +1,5 @@
 window.EXPEDITION_SUPABASE = {
-  url: "COLE_AQUI_A_URL_DO_SUPABASE",
-  anonKey: "COLE_AQUI_A_ANON_KEY_DO_SUPABASE",
+  url: "https://eadqxwbmsnmtwulidhbo.supabase.co",
+  anonKey: "sb_publishable_4J2AwOKHsVC8uZ9C6JuUQg_-hEMuV2P",
   statusId: "default",
 };
